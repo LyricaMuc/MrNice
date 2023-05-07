591b35f4668635a15603906c142512c0db2bcbff
lyrica <lyricatv@gmail.com>
2023-05-07 14:27:53 +0200

    cleanup repo and set global css styles on body

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+/*
+ * Styling
+*/
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
