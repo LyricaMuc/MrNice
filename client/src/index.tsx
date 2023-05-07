49aed3c723ac301a4f8cdbfc488e67f75a90d842
lyrica <lyricatv@gmail.com>
2023-05-07 17:13:09 +0200

    Create modal component and implement the login modal

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,8 +8,11 @@ import { createGlobalStyle } from 'styled-components';
 */
 const GlobalStyle = createGlobalStyle`
   body {
+    background: rgba(59, 113, 202, 1);
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
   }
 `;
 
---
+++ client/src/pages/Welcome.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Modal from '../components/Modal';
+
+/*
+ * Styling
+*/
+const Container = styled.div``;
+
+const Button = styled.button``;
+
+const LoginHeader = styled.h1`
+    text-align: center;
+`;
+
+const LoginText = styled.p`
+    text-align: center;
+`;
+
+const Welcome = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <Container>
+            {isActive &&
+            <Modal isActive={isActive} onClose={() => setIsActive(false)}> 
+                <LoginHeader>Login</LoginHeader>
+                <LoginText>
+                    Bitte melde Dich an, um in den Premium Bereich zu wechseln
+                </LoginText>
+            </Modal>}
+            <Button onClick={() => setIsActive(true)}>Hier geht es zur Anmeldung</Button>
+        </Container>
+
+    );
+};
+
+export default Welcome;
